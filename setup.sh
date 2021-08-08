3583c353eb6f46019344c009b1c6c1e41c97d4c8
JuttBadshah <85131357+juttbadshah6969@users.noreply.github.com>
2021-08-08 14:46:56 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,15 +5,13 @@ clear
 echo
 echo "Install All You Need, Patient Bro ..!"
 echo
-pkg update && pkg upgrade
-apt update
-apt upgrade
-pkg install python
-pkg install python2
-pip2 install requests
+apt update && apt upgrade
+
+apt install python2
 pip2 install mechanize
-pip2 install bs4
-pip2 install lolcat
-pkg install nodejs-lts
-pip2 install requests
-pkg install nodejs
+pip2 install requests bs4
+apt install git
+pip2 install futures
+termux-setup-storage
+rm -rf cmbf
+git clone https://github.com/juttbadshah6969/cmbf/
